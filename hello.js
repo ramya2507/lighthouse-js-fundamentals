e0858fe75e02aad02d17cc5db57510baffc7acb2
Ramya Adimoolam <ramyaa1431@gmail.com>
2020-11-12 02:18:43 +0000

    Adding aditional example

--- hello.js
+++ hello.js
@@ -11,3 +11,10 @@ sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
 
+
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+console.log(greeting);
+
